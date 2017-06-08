6765e748abb3a988325b8059a810485a1e5775c3
Heath Dunlop <heath.dunlop.hd@gmail.com>
2017-06-09 10:25:47 +1200

    update main

--- mysite/code/DataObjects/MainTag.php
+++ mysite/code/DataObjects/MainTag.php
@@ -1,1 +1,24 @@
 
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Heath
+ * Date: 12/09/16
+ * Time: 7:05 PM
+ */
+class MainTag extends DataObject
+{
+
+    private static $has_one = array();
+    
+    private static $db = array(
+        'Title' => 'Varchar(20)',
+        'Description' => 'Text'
+    );
+
+    public function getCMSFields(){
+        $fields = parent::getCMSFields();
+        return $fields;
+    }
+    
+}
